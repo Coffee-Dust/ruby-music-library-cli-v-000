c9c15fc48a3dbc0dbf4296bd402f7c6784e6be28
Jonathan Dust <d24dust@hotmail.com>
2018-07-04 20:17:25 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,19 +1,19 @@
-module Concerns::Findable
-  def find_by_name(name)
-    self.all.detect {|item| item.name == name }
-  end
-
-  def find_or_create_by_name(name)
-    result = self.find_by_name(name)
-    if result == nil
-      self.create(name)
-    else
-      result
-    end
-  end
-
-end#Findable
-
 module Concerns
+
+  module Concerns::Findable
+    def find_by_name(name)
+      self.all.detect {|item| item.name == name }
+    end
+
+    def find_or_create_by_name(name)
+      result = self.find_by_name(name)
+      if result == nil
+        self.create(name)
+      else
+        result
+      end
+    end
+
+  end
 
 end#endof Concerns
